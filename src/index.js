6c4b65d2beb1b88e8be7010f32f44bb8f3d9cd44
Sajeevan98 <sajee91098@gmail.com>
2021-10-10 23:49:28 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const ToDoList = () =>{
+  const toDo = text => updateTask([...task,{text}]);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const[task, updateTask] =useState([
+    {
+      text: "list-01",
+      isCompleted: false
+    },
+    {
+      text: "list-02",
+      isCompleted: false
+    },
+    {
+      text: "list-03",
+      isCompleted: false
+    }
+  ]);
+
+  const AddTask = ({toDo}) =>{
+    const[value, updateValue] = useState(" ");
+
+    const handleSubmit = e =>{
+      e.preventDefault();
+      if(value !==" ")
+      {
+        toDo(value)
+        updateValue(" ");
+      }
+    };
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={value} placeholder="Add Here..." onChange ={ e => updateValue(e.target.value)}/>
+        <button type="submit"><i class="material-icons">add</i></button>
+      </form>
+    );
+  }
+  const toogleTask = iValue =>{
+    const newTask = [...task];
+    if(newTask[iValue].isCompleted)
+    {
+      newTask[iValue].isCompleted = false; 
+    }
+    else
+    {
+      newTask[iValue].isCompleted = true; 
+    }
+    updateTask(newTask);
+  }
+
+  const removeTask = iValue =>{
+    const newTask = [...task];
+    newTask.splice(iValue,1);
+    updateTask(newTask);
+  }
+
+  return (
+    <div className="list-of-TodoList">
+        {
+          task.map((cValue, iValue) =>(
+            <div className="list-view">
+              <span onClick ={ () => toogleTask(iValue) } className={ cValue.isCompleted? "task-name completed-task": "task-name"}>
+                                {cValue.text}
+              </span>
+              <button onClick ={ ()  => removeTask(iValue) }> 
+                <i class="material-icons">delete</i>
+              </button>
+            </div>
+          ))
+        }
+        <AddTask toDo={toDo} />
+    </div>
+  );
+}
+
+ReactDOM.render(<ToDoList/>, document.getElementById('root'));
